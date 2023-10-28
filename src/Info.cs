f10ecd79df29e7121e4beb54bf29261e39ee2e1b
Payn <ivanttt54@gmail.com>
2023-10-28 11:06:14 -0300

    change code indentation

--- src/Info.cs
+++ src/Info.cs
@@ -2,98 +2,98 @@ using System.Text.RegularExpressions;
 
 namespace NyapFetch
 {
-  public static class Info
-  {
-    private static string? s_osInfo;
-    public static string GetOS()
+    public static class Info
     {
-      var file = File.OpenRead("/etc/os-release");
-      var stream = new StreamReader(file);
-
-      while (!stream.EndOfStream)
-      {
-        string? line = stream.ReadLine();
-        if (line != null && line.Contains("PRETTY_NAME"))
+        private static string? s_osInfo;
+        public static string GetOS()
         {
-          s_osInfo = line;
-          break;
+            var file = File.OpenRead("/etc/os-release");
+            var stream = new StreamReader(file);
+
+            while (!stream.EndOfStream)
+            {
+                string? line = stream.ReadLine();
+                if (line != null && line.Contains("PRETTY_NAME"))
+                {
+                    s_osInfo = line;
+                    break;
+                }
+            }
+
+            if (s_osInfo == null)
+            {
+                s_osInfo = "Undefined";
+            }
+            else
+            {
+                s_osInfo = Regex.Replace(s_osInfo, "(PRETTY_NAME\\=)", String.Empty);
+            }
+
+            return s_osInfo;
         }
-      }
 
-      if (s_osInfo == null)
-      {
-        s_osInfo = "Undefined";
-      }
-      else
-      {
-        s_osInfo = Regex.Replace(s_osInfo, "(PRETTY_NAME\\=)", String.Empty);
-      }
+        private static string? s_cpuInfo;
+        public static string GetCPU()
+        {
+            var file = File.OpenRead("/proc/cpuinfo");
+            var stream = new StreamReader(file);
 
-      return s_osInfo;
+            while (!stream.EndOfStream)
+            {
+                string? line = stream.ReadLine();
+                if (line != null && line.Contains("model name"))
+                {
+                    s_cpuInfo = line;
+                    break;
+                }
+            }
+
+            if (s_cpuInfo == null)
+            {
+                s_cpuInfo = "Undefined";
+            }
+            else
+            {
+                s_cpuInfo = Regex.Replace(s_cpuInfo, "(model name	\\: )", String.Empty);
+            }
+
+            return s_cpuInfo;
+        }
+
+        private static string? s_ramInfo;
+        public static string GetRAM()
+        {
+            var file = File.OpenRead("/proc/meminfo");
+            var stream = new StreamReader(file);
+
+            string? ramtotal = null;
+            string? ramfree = null;
+
+            while (!stream.EndOfStream)
+            {
+                string? line = stream.ReadLine();
+
+                if (line != null && line.Contains("MemTotal"))
+                {
+                    ramtotal = line;
+                }
+                if (line != null && line.Contains("MemFree"))
+                {
+                    ramfree = line;
+                }
+            }
+
+            ramtotal = Regex.Replace(ramtotal, "(MemTotal\\:)", String.Empty).Trim();
+            ramfree = Regex.Replace(ramfree, "(MemFree\\:)", String.Empty).Trim();
+
+            s_ramInfo = ramfree + " / " + ramtotal;
+
+            if (s_ramInfo == null)
+            {
+                s_ramInfo = "Undefined";
+            }
+
+            return s_ramInfo;
+        }
     }
-
-    private static string? s_cpuInfo;
-    public static string GetCPU()
-    {
-      var file = File.OpenRead("/proc/cpuinfo");
-      var stream = new StreamReader(file);
-
-      while (!stream.EndOfStream)
-      {
-        string? line = stream.ReadLine();
-        if (line != null && line.Contains("model name"))
-        {
-          s_cpuInfo = line;
-          break;
-        }
-      }
-      
-      if (s_cpuInfo == null)
-      {
-        s_cpuInfo = "Undefined";
-      }
-      else
-      {
-        s_cpuInfo = Regex.Replace(s_cpuInfo, "(model name	\\: )", String.Empty);
-      }
-
-      return s_cpuInfo;
-    }
-
-    private static string? s_ramInfo;
-    public static string GetRAM()
-    {
-      var file = File.OpenRead("/proc/meminfo");
-      var stream = new StreamReader(file);
-
-      string? ramtotal = null;
-      string? ramfree = null;
-
-      while (!stream.EndOfStream)
-      {
-        string? line = stream.ReadLine();
-        
-        if (line != null && line.Contains("MemTotal"))
-        {
-           ramtotal = line;
-        }
-        if (line != null && line.Contains("MemFree"))
-        {
-          ramfree = line;
-        }
-      }
-      
-      ramtotal = Regex.Replace(ramtotal, "(MemTotal\\:)", String.Empty).Trim();
-      ramfree = Regex.Replace(ramfree, "(MemFree\\:)", String.Empty).Trim();
-
-      s_ramInfo = ramfree + " / " + ramtotal;
-
-      if (s_ramInfo == null)
-      {
-        s_ramInfo = "Undefined";
-      }
-
-      return s_ramInfo;
-    }
-  }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -2,9 +2,9 @@
 
 namespace NyapFetch
 {
-  public class Program
-  {
-    private static string s_sysInfo = $"""
+    public class Program
+    {
+        private static string s_sysInfo = $"""
         ⣿⣿⣿⡷⠊⡢⡹⣦⡑⢂⢕⢂⢕⢂⢕⢂⠕⠔⠌⠝⠛⠶⠶⢶⣦⣄⢂⢕⢂⢕ 
         ⣿⣿⠏⣠⣾⣦⡐⢌⢿⣷⣦⣅⡑⠕⠡⠐⢿⠿⣛⠟⠛⠛⠛⠛⠡⢷⡈⢂⢕⢂
         ⠟⣡⣾⣿⣿⣿⣿⣦⣑⠝⢿⣿⣿⣿⣿⣿⡵⢁⣤⣶⣶⣿⢿⢿⢿⡟⢻⣤⢑⢂ 
@@ -16,29 +16,29 @@ namespace NyapFetch
         ⡣⡘⢄⠙⣾⣾⣾⣿⣿⣿⣿⣿⣿⡀⢐⢕⢕⢕⢕⢕⡘⣿⣿⣿⣿⣿⣿⠏⠠⠈
         ⠌⢊⢂⢣⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢐⢕⢕⢕⢕⢕⢅⣿⣿⣿⣿⡿⢋⢜⠠⠈
         """;
-    
-    private static void WriteColor(string message, ConsoleColor color)
-    {
-      var pieces = Regex.Split(message, @"(\[[^\]]*\])");
 
-      for(int i = 0; i < pieces.Length; i++)
-      {
-        string piece = pieces[i];
-        
-        if (piece.StartsWith("[") && piece.EndsWith("]"))
+        private static void WriteColor(string message, ConsoleColor color)
         {
-          Console.ForegroundColor = color;
-          piece = piece.Substring(1, piece.Length - 2);          
-        }
-        Console.Write(piece);
-        Console.ResetColor();
-      }
-      Console.WriteLine();
-    }
+            var pieces = Regex.Split(message, @"(\[[^\]]*\])");
 
-    public static void Main()
-    {
-      WriteColor(s_sysInfo, ConsoleColor.Blue);
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                string piece = pieces[i];
+
+                if (piece.StartsWith("[") && piece.EndsWith("]"))
+                {
+                    Console.ForegroundColor = color;
+                    piece = piece.Substring(1, piece.Length - 2);
+                }
+                Console.Write(piece);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main()
+        {
+            WriteColor(s_sysInfo, ConsoleColor.Blue);
+        }
     }
-  }
 }
